7e8fce8bbcf132d2699034356a2f3204f723ac4c
albertgoncalves <goncalves.albert@gmail.com>
2019-05-04 20:02:27 -0400

    more

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function Player(canvas, x, y, height, width, speed) {
+function Player(x, y, height, width, speed) {
     this.width = width;
     this.height = height;
     this.x = x;
@@ -6,55 +6,64 @@ function Player(canvas, x, y, height, width, speed) {
     this.speed = speed;
     this.minX = 0;
     this.minY = 0;
-    this.maxX = canvas.width - width;
-    this.maxY = canvas.height - height;
+    this.maxX = CANVAS.width - width;
+    this.maxY = CANVAS.height - height;
 }
 
 function Time() {
     this.accu = 0;
     this.speed = 0.25;
+    this.now = Date.now();
 }
 
 function drawBackground() {
-    ctx.fillStyle = "hsl(" + String(time.accu) + ", 10%, 25%)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    CTX.fillStyle = "hsl(" + String(TIME.accu) + ", 10%, 25%)";
+    CTX.fillRect(0, 0, CANVAS.width, CANVAS.height);
 }
 
 function drawPlayer() {
-    ctx.strokeStyle = "white";
-    ctx.lineWidth = 2;
-    ctx.strokeRect(player.x, player.y, player.width, player.height);
+    CTX.strokeStyle = "white";
+    CTX.lineWidth = 2;
+    CTX.strokeRect(PLAYER.x, PLAYER.y, PLAYER.width, PLAYER.height);
 }
 
 function flowTime() {
-    time.accu += time.speed;
-    if (time.accu % 255 == 0) {
-        time.speed *= -1;
+    TIME.accu += TIME.speed;
+    if (TIME.accu % 255 == 0) {
+        TIME.speed *= -1;
     }
 }
 
-function keyInput(event) {
+function updateFPS() {
+    if (TIME.accu % 5 == 0) {
+        elapsed = 1 / ((Date.now() - TIME.now) / 1000);
+        FPS.innerHTML = String(Math.floor(elapsed)) + " fps";
+    }
+    TIME.now = Date.now();
+}
+
+function keyDown(event) {
     switch(event.keyCode) {
-        case 37:
-            if (player.x > player.minX) {
-                player.x -= player.speed;
-            }
-            break;
-        case 38:
-            if (player.y > player.minY) {
-                player.y -= player.speed;
-            }
-            break;
-        case 39:
-            if (player.x < player.maxX) {
-                player.x += player.speed;
-            }
-            break;
-        case 40:
-            if (player.y < player.maxY) {
-                player.y += player.speed;
-            }
-            break;
+    case 37:
+        if (PLAYER.x > PLAYER.minX) {
+            PLAYER.x -= PLAYER.speed;
+        }
+        break;
+    case 38:
+        if (PLAYER.y > PLAYER.minY) {
+            PLAYER.y -= PLAYER.speed;
+        }
+        break;
+    case 39:
+        if (PLAYER.x < PLAYER.maxX) {
+            PLAYER.x += PLAYER.speed;
+        }
+        break;
+    case 40:
+        if (PLAYER.y < PLAYER.maxY) {
+            PLAYER.y += PLAYER.speed;
+        }
+        break;
     }
 }
 
@@ -62,14 +71,16 @@ function loop() {
     flowTime();
     drawBackground();
     drawPlayer();
+    updateFPS();
 }
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var player = new Player(canvas, 90, 90, 30, 30, 15);
-var time = new Time();
+var CANVAS = document.getElementById("canvas");
+var FPS = document.getElementById("fps");
+var CTX = CANVAS.getContext("2d");
+var PLAYER = new Player(90, 90, 30, 30, 15);
+var TIME = new Time();
 
 window.onload = function() {
-    document.addEventListener("keydown", keyInput);
+    document.addEventListener("keydown", keyDown);
     setInterval(loop, 1000 / 60);
 };
