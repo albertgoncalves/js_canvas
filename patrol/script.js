8b99929fe05e4a411d39be6535d4cd731c53c1f1
albertgoncalves <goncalves.albert@gmail.com>
2019-05-12 01:25:00 -0400

    nice

--- patrol/script.js
+++ patrol/script.js
@@ -1,14 +1,16 @@
 var WIDTH = 500;
 var HEIGHT = 500;
 var SCALE = 225;
-var SPEED = 0.002;
+var SPEED = 0.00025;
 var K = 0.55;
 var RADIUS = 25;
 
 var canvas;
 var context;
 var color = 0;
-var flow = 1;
+var colorFlow = 1;
+var alpha = 90;
+var alphaFlow = 1;
 
 function init() {
     canvas = document.createElement('canvas');
@@ -20,16 +22,24 @@ function init() {
 
 function cycleColor() {
     if ((color > 255) || (color < 0)) {
-        flow *= -1;
+        colorFlow *= -1;
     }
-    color += flow;
+    color += colorFlow;
+}
+
+function cycleAlpha() {
+    if ((alpha > 90) || (alpha < 20)) {
+        alphaFlow *= -1;
+    }
+    alpha += alphaFlow;
 }
 
 function drawCircle(z) {
     var time = Date.now() * SPEED;
     var x = (Math.sin(time + z) * SCALE) + (WIDTH / 2);
     var y = (Math.cos((time + z) * K) * SCALE) + (HEIGHT / 2);
-    context.fillStyle = "hsl(" + String(color + z) + ", 75%, 65%)";
+    context.fillStyle =
+        "hsla(" + String(color) + ", 75%, 65%, " + String(alpha) + "%)";
     context.beginPath();
     context.arc(x, y, RADIUS, 0, Math.PI * 2, true);
     context.closePath();
@@ -38,16 +48,17 @@ function drawCircle(z) {
 
 function loop() {
     requestAnimationFrame(loop);
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, WIDTH, HEIGHT);
     drawCircle(0);
     drawCircle(10);
+    drawCircle(25);
     drawCircle(50);
-    drawCircle(100);
-    drawCircle(105);
+    drawCircle(75);
+    drawCircle(90);
     drawCircle(200);
     drawCircle(250);
-    drawCircle(255);
     cycleColor();
+    cycleAlpha();
 }
 
 init();
