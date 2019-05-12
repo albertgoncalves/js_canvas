a7804b45de03c5b72da8d7bb1cbee4994d1c08c9
albertgoncalves <goncalves.albert@gmail.com>
2019-05-12 15:15:38 -0400

    seems to work

--- followers/script.js
+++ followers/script.js
@@ -1,31 +1,26 @@
 var WIDTH = 700;
 var HEIGHT = 700;
 var SCALE = 220;
-var SPEED = 1 / 250;
+var SPEED = 1 / 150;
 var K = 0.55;
-var RADIUS = 30;
+var RADIUS = 50;
 var COLOR = {"max": 255, "min": 0};
 var ALPHA = {"max": 90, "min": 20};
 
 var canvas;
 var context;
-var color = COLOR.min;
-var colorDelta = 0.15;
-var alpha = ALPHA.max;
-var alphaDelta = 0.15;
-var mouseX = WIDTH / 2;
-var mouseY = HEIGHT / 2;
-var n = 25;
+var color = {"value": COLOR.min, "delta": 0.15};
+var alpha = {"value": ALPHA.max, "delta": 0.15};
+var mouse = {"x": WIDTH / 2, "y": HEIGHT / 2};
+var n = 9;
 var squares = new Array(n);
 for (var i = 0; i < n; i++) {
     squares[i] = new Square();
 }
 
 function Square() {
-    this.x = Math.floor(Math.random() * WIDTH);
-    this.y = Math.floor(Math.random() * HEIGHT);
-    this.targetX = Math.floor(Math.random() * WIDTH);
-    this.targetY = Math.floor(Math.random() * HEIGHT);
+    this.x = Math.floor(Math.random() * (WIDTH - (RADIUS * 2))) + RADIUS;
+    this.y = Math.floor(Math.random() * (HEIGHT - (RADIUS * 2))) + RADIUS;
 }
 
 function init() {
@@ -50,39 +45,51 @@ function distance(aX, aY, bX, bY) {
     return Math.sqrt(Math.pow(bX - aX, 2) + Math.pow(bY - aY, 2));
 }
 
-function move(a, b, c, d, limit) {
-    if ((a + RADIUS) > limit) {
-        return a - 1;
-    } else if ((a - RADIUS) < 0) {
-        return a + 1;
+function checkBounds(pos, limit) {
+    if ((pos + RADIUS) > limit) {
+        return pos - 1;
+    } else if ((pos - RADIUS) < 0) {
+        return pos + 1;
     } else {
-        var r = Math.random();
-        if (r > 0.85) {
-            return a + ((b - a) * SPEED);
-        } else if (r > 0.55) {
-            return a - ((c - a) * SPEED);
-        } else {
-            return a + ((d - a) * SPEED);
-        }
+        return pos;
     }
 }
 
+function move(pos, near, mouse) {
+    var next = {
+        "x": pos.x + ((mouse.x - pos.x) * SPEED),
+        "y": pos.y + ((mouse.y - pos.y) * SPEED),
+    };
+    var threshold = distance(pos.x, pos.y, mouse.x, mouse.y);
+    var gap = distance(pos.x, pos.y, near.x, near.y);
+    if (Math.random() < ((RADIUS * 2) / (threshold + gap))) {
+        if (distance(pos.x, pos.y, near.x, near.y) < (RADIUS * 2)) {
+            return {
+                "x": pos.x - ((near.x - pos.x) * SPEED * 3),
+                "y": pos.y - ((near.y - pos.y) * SPEED * 3),
+            };
+        }
+    }
+    return next;
+}
+
 function animateSquare(square) {
-    context.fillStyle = formatColor(color, alpha);
+    context.fillStyle = formatColor(color.value, alpha.value);
     drawSquare(square.x, square.y);
-    var delta = 0;
-    j = 0;
+    var delta = distance(0, 0, WIDTH, HEIGHT);
+    var j = 0;
     for (var i in squares) {
         if (square != squares[i]) {
             next = distance(square.x, square.y, squares[i].x, squares[i].y);
-            if (next > delta) {
+            if (next < delta) {
                 delta = next;
                 j = i;
             }
         }
     }
-    square.x = move(square.x, square.targetX, squares[j].x, mouseX, WIDTH);
-    square.y = move(square.y, square.targetY, squares[j].y, mouseY, HEIGHT);
+    var xy = move(square, squares[j], mouse);
+    square.x = checkBounds(xy.x, WIDTH);
+    square.y = checkBounds(xy.y, HEIGHT);
 }
 
 function cycle(value, delta, min, max) {
@@ -99,18 +106,14 @@ function loop() {
     for (var i in squares) {
         animateSquare(squares[i]);
     }
-    colorCycle = cycle(color, colorDelta, COLOR.min, COLOR.max);
-    color = colorCycle.value;
-    colorDelta = colorCycle.delta;
-    alphaCycle = cycle(alpha, alphaDelta, ALPHA.min, ALPHA.max);
-    alpha = alphaCycle.value;
-    alphaDelta = alphaCycle.delta;
+    color = cycle(color.value, color.delta, COLOR.min, COLOR.max);
+    alpha = cycle(alpha.value, alpha.delta, ALPHA.min, ALPHA.max);
 }
 
 function mousePosition(event) {
     bounds = canvas.getBoundingClientRect();
-    mouseX = event.clientX - bounds.left;
-    mouseY = event.clientY - bounds.top;
+    mouse.x = event.clientX - bounds.left;
+    mouse.y = event.clientY - bounds.top;
 }
 
 window.onload = function() {
